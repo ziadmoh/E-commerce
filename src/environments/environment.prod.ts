559e2952e767d53a5b7a7e7d741ee7d70018007a
ziad yahya <ziadyahya01001@gmail.com>
2022-08-22 21:02:25 +0200

    edit api link

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   appName:'Imagine',
   demo: 'demo1',
-  SERVER_URL: 'http://apis.imagineeg.com/',
+  SERVER_URL: 'https://imagineapp.azurewebsites.net/',
   FaceBookAppId:'1044523549507440',
   GoogleAppClientId:'774574771657-mpjd6avv1qndfrpr841osts3n35rmrik.apps.googleusercontent.com',
   GoogleAppClientSecret:'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',
